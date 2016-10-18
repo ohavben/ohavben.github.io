5d499110b254149b454bac59721e3e91638b4699
ohavben <ohavben@gmail.com>
2016-10-18 11:54:57 -0700

    x

--- main.js
+++ main.js
@@ -215,7 +215,7 @@
         var NotesPosition = function(){
             var width = window.innerWidth, height = window.innerHeight, result;
                 if(width/height > 1){ //landscape
-                    result = [ '8%',  '24%', '-5%',  '36%',  '30%', '75%', '30%', '6%', '30%', '28%', '47%', '65%' ];
+                    result = [ '8%',  '24%', '-5%',  '36%',  '30%', '71%', '30%', '6%', '30%', '28%', '47%', '65%' ];
                 }
                 else { //portrait
                     result = ['7%',  '4%', '-2%',  '24%', '55%', '6%', '55%', '55%', '22%', '12%', '65%', '60%' ];
